f57b2d10821bd030dcc4886a49ed84a2cf981317
capeta0507 <nintendof1@gmail.com>
2020-09-24 11:40:27 +0800

    first commit

---
+++ webChat.js
@@ -0,0 +1,58 @@
+const webMessage = [
+  {
+    "no":1,
+    "name": "David",
+    "photo":"/img/friends/David.png",
+    "type":"text",
+    "message":"Hi",
+    "dateTime":"09/23 10:00"
+  },
+  {
+    "no":2,
+    "name": "Johnny",
+    "photo":"/img/me/Johnny.png",
+    "type":"text",
+    "message":"你好",
+    "dateTime":"09/23 10:15"
+  },
+  {
+    "no":3,
+    "name": "David",
+    "photo":"/img/friends/David.png",
+    "type":"text",
+    "message":"推薦給你一張漂亮的底圖...",
+    "dateTime":"09/23 10:16"
+  },
+  {
+    "no":4,
+    "name": "Johnny",
+    "photo":"/img/me/Johnny.png",
+    "type":"text",
+    "message":"好ㄜ",
+    "dateTime":"09/23 10:18"
+  },
+  {
+    "no":5,
+    "name": "David",
+    "photo":"/img/friends/David.png",
+    "type":"photo",
+    "message":"/img/photo/423893.jpg",
+    "dateTime":"09/23 10:19"
+  },
+  {
+    "no":6,
+    "name": "Johnny",
+    "photo":"/img/me/Johnny.png",
+    "type":"text",
+    "message":"謝謝，我將用在新的專案上...",
+    "dateTime":"09/23 10:20"
+  },
+  {
+    "no":7,
+    "name": "David",
+    "photo":"/img/emoji/like.png",
+    "type":"emoji",
+    "message":"推薦給你一張漂亮的底圖...",
+    "dateTime":"09/23 10:16"
+  }
+]
