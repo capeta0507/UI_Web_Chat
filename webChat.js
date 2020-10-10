16a55e7734eb7f4d4cd69eda9f298928d1ffb434
詹政勳 <nintendof1@gmail.com>
2020-10-10 22:26:53 +0800

    sdas

--- webChat.js
+++ webChat.js
@@ -5,7 +5,8 @@ const webMessage = [
     "photo":"/img/friends/David.png",
     "type":"text",
     "message":"Hi",
-    "dateTime":"09/23 10:00"
+    "dateTime":"09/23 10:00",
+    "me": false
   },
   {
     "no":2,
@@ -13,7 +14,8 @@ const webMessage = [
     "photo":"/img/me/Johnny.png",
     "type":"text",
     "message":"你好",
-    "dateTime":"09/23 10:15"
+    "dateTime":"09/23 10:15",
+    "me": true
   },
   {
     "no":3,
@@ -21,7 +23,8 @@ const webMessage = [
     "photo":"/img/friends/David.png",
     "type":"text",
     "message":"推薦給你一張漂亮的底圖...",
-    "dateTime":"09/23 10:16"
+    "dateTime":"09/23 10:16",
+    "me": false
   },
   {
     "no":4,
@@ -29,7 +32,8 @@ const webMessage = [
     "photo":"/img/me/Johnny.png",
     "type":"text",
     "message":"好ㄜ",
-    "dateTime":"09/23 10:18"
+    "dateTime":"09/23 10:18",
+    "me": true
   },
   {
     "no":5,
@@ -37,7 +41,8 @@ const webMessage = [
     "photo":"/img/friends/David.png",
     "type":"photo",
     "message":"/img/photo/423893.jpg",
-    "dateTime":"09/23 10:19"
+    "dateTime":"09/23 10:19",
+    "me": false
   },
   {
     "no":6,
@@ -45,7 +50,8 @@ const webMessage = [
     "photo":"/img/me/Johnny.png",
     "type":"text",
     "message":"謝謝，我將用在新的專案上...",
-    "dateTime":"09/23 10:20"
+    "dateTime":"09/23 10:20",
+    "me": true
   },
   {
     "no":7,
@@ -53,6 +59,56 @@ const webMessage = [
     "photo":"/img/friends/David.png",
     "type":"emoji",
     "message":"/img/emoji/like.png",
-    "dateTime":"09/23 10:16"
+    "dateTime":"09/23 10:16",
+    "me": false
   }
 ]
+
+let message = '';
+webMessage.map(data => {
+  if(data.me){
+    if(data.type == 'text'){
+      message += `
+        <div class="message_row you-message">
+          <div class="message-content">
+            <div class="message-text">${data.message}</div>
+            <div class="message-time">${data.dateTime}</div>
+          </div>
+        </div>
+      `
+    } else {
+      message += `
+        <div class="message_row you-message">
+          <div class="message-content">
+            <img class="ejIcon" src="${data.message}" alt="">
+            <div class="message-time">${data.dateTime}</div>
+          </div>
+        </div>
+      `
+    }
+  } else {
+    if(data.type == 'text'){
+      message += `
+        <div class="message_row other-message">
+          <div class="message-content">
+            <img class="head" src="${data.photo}" alt="">
+            <div class="message-text">${data.message}</div>
+            <div class="message-time">${data.dateTime}</div>
+          </div>
+        </div>
+      `
+    } else {
+      message += `
+        <div class="message_row other-message">
+          <div class="message-content">
+            <img class="head" src="${data.photo}" alt="">
+            <img class="ejIcon" src="${data.message}" alt="">
+            <div class="message-time">${data.dateTime}</div>
+          </div>
+        </div>
+      `
+    }
+  }
+})
+
+document.getElementById('chatRoom').innerHTML = message;
